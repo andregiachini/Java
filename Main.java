d8a42c6cd29ff7ce1396e104727ed387abf028bb
andregiachini <andrelucas_giachini@hotmail.com>
2022-09-25 18:31:31 -0300

    update

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import javax.sound.sampled.SourceDataLine;
 
 public class Main{
     public static void main(String[] args) {
@@ -5,10 +6,10 @@ public class Main{
         System.out.println("EXERCICIO 01");
         System.out.println("--------------------------------------------------------");
         System.out.println("Programação I");
-        System.out.println("\t Professora: Andressa Sebben");
-        System.out.println("\t Aluno: André Giachini");
-        System.out.println("\t Objetivo: \n\t\t O objetivo dessa disciplina é compreender os conceitos fundamentais\n\t\t do paradigma de programaçao orientada a objetos e aplicá-los no\n\t\t desenvolvimento de soluções de software.");
-        System.out.println("\t Turno: Noturno");
+        System.out.println("\t -Professora: Andressa Sebben");
+        System.out.println("\t -Aluno: André Giachini");
+        System.out.println("\t -Objetivo: \n\t\t O objetivo dessa disciplina é compreender os conceitos fundamentais\n\t\t do paradigma de programaçao orientada a objetos e aplicá-los no\n\t\t desenvolvimento de soluções de software.");
+        System.out.println("\t -Turno: Noturno");
 
         System.out.println("--------------------------------------------------------");
 
@@ -24,6 +25,73 @@ public class Main{
         System.out.println("A soma dos números inteiros é: " + soma);
         System.out.println("A soma dos números de ponto flutuante é: " + soma2);
         System.out.println("--------------------------------------------------------");
+
+
+        //Exercicio 03
+        System.out.println("EXERCICIO 03");
+        System.out.println("--------------------------------------------------------");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " x " + 5 + " = " + i*5);
+            
+        }
+        System.out.println("--------------------------------------------------------");
+
+        //Exercicio 04
+        System.out.println("EXERCICIO 04");
+        System.out.println("--------------------------------------------------------");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " x " + 6 + " = " + i*6);
+            System.out.println("\t\t" + i + " x " + 2 + " = " + i*2);
+        }
+        System.out.println("--------------------------------------------------------");
+
+
+
+        //Exercicio 05
+        System.out.println("EXERCICIO 05");
+        System.out.println("--------------------------------------------------------");
+
+        
+        for (int i = 1; i < 11; i++) {
+            if (i == 1) {
+                System.out.println(i + " Java atrapalha muita gente");                
+            }
+            else {
+                System.out.print(i + " Java ");
+
+                for (int l = 1; l < i+1; l++) {
+                    System.out.print("atrapalham ");             
+                }
+
+                System.out.println("muita gente");
+            }
+        }
+        System.out.println("--------------------------------------------------------");
+
+        //Exercicio 06
+        System.out.println("EXERCICIO 06");
+        System.out.println("--------------------------------------------------------");
+
+        
+
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
