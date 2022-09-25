173c6a7bd0817ee093080e84412ca10382249394
andregiachini <andrelucas_giachini@hotmail.com>
2022-09-25 17:30:58 -0300

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //EXERCICIO 013
+        System.out.println("EXERCICIO 01");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Programação I");
+        System.out.println("\t Professora: Andressa Sebben");
+        System.out.println("\t Aluno: André Giachini");
+        System.out.println("\t Objetivo: \n\t\t O objetivo dessa disciplina é compreender os conceitos fundamentais\n\t\t do paradigma de programaçao orientada a objetos e aplicá-los no\n\t\t desenvolvimento de soluções de software.");
+        System.out.println("\t Turno: Noturno");
+
+        System.out.println("--------------------------------------------------------");
+
+        //EXERICIO 02
+        System.out.println("EXERCICIO 02");
+        System.out.println("--------------------------------------------------------");
+        int x = 4;
+        int y = 5;
+        double j = 4.5;
+        double k = 5.4;
+        int soma = x + y;
+        double soma2 = j + k;
+        System.out.println("A soma dos números inteiros é: " + soma);
+        System.out.println("A soma dos números de ponto flutuante é: " + soma2);
+        System.out.println("--------------------------------------------------------");
+
     }
 }
