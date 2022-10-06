b327409de5fefe4574e241f54f06432328cee9ad
andregiachini <andrelucas_giachini@hotmail.com>
2022-10-06 20:32:16 -0300

    update

--- Main.java
+++ Main.java
@@ -124,7 +124,6 @@ public class Main{
 
         System.out.println("EXERCICIO 08");
         System.out.println("--------------------------------------------------------");
-
         
         int vetor[] = new int[10];
 
