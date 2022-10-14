31618b635283a50b7468ea764895ff1bbe735908
andregiachini <andrelucas_giachini@hotmail.com>
2022-10-13 22:06:43 -0300

    update

--- apolice/Apolice.java
+++ apolice/Apolice.java
@@ -1,75 +1,60 @@
 package apolice;
 
 public class Apolice {
-    int numApolice;
-    String nomeSegurado;
-    int idade;
-    float valorPremio;
-    int ultimoNumApolice = 0;
-
-    public void Apolice(){
-        this.ultimoNumApolice++;
-        this.numApolice = this.ultimoNumApolice;
-
+    public int numApolice;
+    public String nomeSegurado;
+    public int idade;
+    public float valorPremio;
+    public static int ultimoNumApolice = 0;
+    
+    public Apolice(){
+        ultimoNumApolice++;
+        this.numApolice = ultimoNumApolice;
     }
-
-    public void Apolice(String nomeSegurado, int idade, float valorPremio){
-        this.ultimoNumApolice++;
-        this.numApolice = this.ultimoNumApolice;
+    public Apolice(String nomeSegurado, int idade, float valorPremio){
+        ultimoNumApolice++;
+        this.numApolice = ultimoNumApolice;
         this.nomeSegurado = nomeSegurado;
         this.valorPremio = valorPremio;
-
-
     }
-
     public void imprimir(){
-        System.out.println("Número da apólice: " + this.numApolice);
-        System.out.println("Nome do Segurado: " + this.nomeSegurado);
-        System.out.println("Idade: " + this.idade);
-        System.out.println("Valor Prêmio: " + this.valorPremio);
-
+        System.out.println("----------------------------------");
+        System.out.println(this.numApolice);
+        System.out.println(this.nomeSegurado);
+        System.out.println(this.idade);
+        System.out.println(this.valorPremio);
+        System.out.println("---------------------------------");
     }
-
     public void calcularPremio(){
-        if (this.idade >= 18 && this.idade <= 25) {
-            this.valorPremio = this.valorPremio * 1.2f;
-
-        }else if (this.idade > 25 && this.idade <= 36) {
-            this.valorPremio *= 1.15f; 
-            
-        }else if (this.idade > 36) {
-            this.valorPremio *= 1.1f;
-            
-        }   
-    }
-
-    public float oferecerDesconto(String cidade){
-        if (cidade == "Florianópolis") {
-            this.valorPremio = this.valorPremio * 0.95f;
-
-            
-        }   else if (cidade == "Chapecó") {
-            this.valorPremio = this.valorPremio * 0.94f;
-
-
-        } else if (cidade == "Joaçaba") {
-            this.valorPremio = this.valorPremio * 0.93f;
-
-
-        } else if (cidade == "Joinville") {
-            this.valorPremio *= 0.92f;
-
+        if(idade >= 18 && idade <= 25){
+            this.valorPremio = (float) (this.valorPremio * 1.20);
+        }else if(idade > 25 && idade <= 36){
+            this.valorPremio = (float) (this.valorPremio * 1.15);
+        }else if(idade > 36){
+            this.valorPremio = (float) (this.valorPremio * 1.10);
         }
-        
-        return this.valorPremio;
     }
-
-    public void exibeUltimoNum(){
-        System.out.println("Número da última apólice criada: " + ultimoNumApolice);
-
+    public float oferecerDesconto(String cidade){
+        switch (cidade) {
+            case "Florianópolis" -> {
+                return (float) (this.valorPremio * 0.05);
+            }
+            case "Chapecó" -> {
+                return (float) (this.valorPremio * 0.06);
+            }
+            case "Joaçaba" -> {
+                return (float) (this.valorPremio * 0.07);
+            }
+            case "Joinville" -> {
+                return (float) (this.valorPremio * 0.08);
+            }
+            default -> {
+            }
+        }
+        return 0;
     }
-
+    public static void exibeUltimoNum(){
+        System.out.println(ultimoNumApolice);
+    }
     
-
-
 }
--- apolice/Main.java
+++ apolice/Main.java
@@ -1,27 +1,24 @@
 package apolice;
 
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Apolice a1 = new Apolice();
-        Scanner s = new Scanner(System.in);
-
-        System.out.println("Digite o nome do segurado: ");
-        a1.nomeSegurado = s.nextLine();
-        System.out.println("Digite a idade do segurado: ");
-        a1.idade = s.nextInt();
-        System.out.println("Digite o valor do prêmio: ");
-        a1.valorPremio = s.nextFloat();
-        System.out.println("DIgite a cidade: ");
-        String cidade = s.nextLine();
-
-        a1.calcularPremio();
-        
-        a1.imprimir();
-
-        a1.oferecerDesconto(cidade);
-
-        a1.imprimir();
+    public static void main(String[] args){
+        String cidade;
+        Scanner scan = new Scanner(System.in);
+        Apolice apolice1 = new Apolice();
+        cidade = scan.nextLine();
+        apolice1.nomeSegurado = scan.nextLine();
+        apolice1.idade = scan.nextInt();
+        apolice1.valorPremio = scan.nextFloat();
+        apolice1.calcularPremio();
+        apolice1.imprimir();
+        float desconto = apolice1.oferecerDesconto(cidade);
+        System.out.println(desconto);
+        Apolice apolice2 = new Apolice("Joao", 18, 1500);
+        apolice2.calcularPremio();
+        apolice2.imprimir();
+        float desconto2 = apolice2.oferecerDesconto("Joaçaba");
+        System.out.println(desconto2);
+        System.out.println(Apolice.ultimoNumApolice);
     }
 }
