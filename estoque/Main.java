2fe3a0b331d6deb5a2acdea2171ae4e21793d673
andregiachini <andrelucas_giachini@hotmail.com>
2022-11-03 21:33:30 -0300

    update

--- estoque/Main.java
+++ estoque/Main.java
@@ -1,5 +1,9 @@
 package estoque;
 
 public class Main {
-    
+  public static void main(String[] args) {
+        Material m1 = new Material(10, "prego", 5);
+
+        Material m2 = new Material(20, "parafuso");
+  }  
 }
